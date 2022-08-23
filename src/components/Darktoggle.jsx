2ecbc728a2f5d89801af92d1f4f1435d1963cb85
gedeapriana <gedeapriana36@gmail.com>
2022-08-23 23:08:37 +0800

    finish dark-mode toggle

--- src/components/Darktoggle.jsx
+++ src/components/Darktoggle.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { Moon, BrightnessLow } from "react-bootstrap-icons";
+import $ from "jquery";
 
 const Darktoggle = () => {
+  // $(".darktoggle-root").on("click", function () {
+  //   $(".toggle").toggleClass("left-0");
+  //   $(".toggle").toggleClass("right-auto");
+  // });
+
+  // const toggleRoot = document.querySelector(".darktoggle-root");
+
+  // toggleRoot.addEventListener("click", function () {
+  //   console.log("OK");
+  // });
+
+  const toggleListener = () => {
+    $(".toggle").toggleClass("left-0");
+    $(".toggle").toggleClass("left-auto");
+    $(".toggle").toggleClass("right-0");
+    $(".toggle").toggleClass("right-auto");
+    $(".darktoggle-root").toggleClass("bg-slate-700");
+  };
+
   return (
-    <div className="darktoggle-root w-16 h-8 bg-slate-50 ml-auto md:ml-4 mx-4 rounded-full relative flex justify-between items-center px-1">
-      <div className="toggle absolute h-6 w-6 rounded-full bg-red-400 top-0 left-0 bottom-0 right-0 m-auto"></div>
+    <div
+      onClick={toggleListener}
+      className="darktoggle-root w-16 h-8 bg-slate-50 ml-auto md:ml-4 mx-4 rounded-full relative flex justify-between items-center px-1 cursor-pointer transition duration-500"
+    >
+      <div className="toggle absolute h-6 w-6 rounded-full bg-red-400 top-0 left-0 bottom-0 right-auto my-auto mx-1 transition duration-500"></div>
       <div className="moon">
-        <Moon className="block" />
+        <Moon className="block text-slate-50" />
       </div>
       <div className="sun">
-        <BrightnessLow className="block text-2xl" />
+        <BrightnessLow className="block text-2xl text-yellow-300" />
       </div>
     </div>
   );
